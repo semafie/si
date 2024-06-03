1a2e991cc2131913c100333d4630592fcc7be2a3
semafie <restuimamsafii04@gmail.com>
2024-06-03 23:15:28 +0700

    gudang

---
+++ database/migrations/2024_05_15_091127_pembelian.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pembelian', function (Blueprint $table) {
+            $table->id();
+            $table->date('tanggal');
+            $table->time('jam');
+            $table->String('total_harga');
+            $table->String('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pembelian');
+    }
+};
---
+++ database/migrations/2024_05_15_091841_detail_pembelian.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('detail_pembelian', function (Blueprint $table) {
+            $table->id();
+            $table->String('nama_obat');
+            $table->integer('harga_obat');
+            $table->integer('jumlah_stok');
+            $table->integer('sub_total');
+            $table->integer('id_pembelian');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detail_pembelian');
+    }
+};
